968473df8b86426385f07c0677a19a23eae0586e
Maarten van Vliet <maartenvanvliet@users.noreply.github.com>
2022-10-11 21:59:05 +0200

    Add typespecs

--- lib/stripe/open_api/phases/compile.ex
+++ lib/stripe/open_api/phases/compile.ex
@@ -4,7 +4,7 @@ defmodule Stripe.OpenApi.Phases.Compile do
     modules = Enum.map(blueprint.components, fn {_k, component} -> component.module end)
 
     for {_name, component} <- blueprint.components do
-      funcs =
+      funcs_types =
         for operation <- component.operations,
             operation_definition =
               lookup_operation(
@@ -17,8 +17,8 @@ defmodule Stripe.OpenApi.Phases.Compile do
             |> Enum.map(&String.to_atom(&1.name))
 
           params? =
-            operation_definition.query_parameters != [] ||
-              operation_definition.body_parameters != []
+            match?({:object, _, [_ | _]}, operation_definition.query_parameters) ||
+              match?({:object, _, [_ | _]}, operation_definition.body_parameters)
 
           argument_names =
             arguments
@@ -54,83 +54,85 @@ defmodule Stripe.OpenApi.Phases.Compile do
 
           params =
             cond do
-              operation_definition.query_parameters != [] -> operation_definition.query_parameters
-              operation_definition.body_parameters != [] -> operation_definition.body_parameters
-              true -> []
+              operation_definition.query_parameters != {:object, [], []} ->
+                operation_definition.query_parameters
+
+              operation_definition.body_parameters != {:object, [], []} ->
+                operation_definition.body_parameters
+
+              true ->
+                []
             end
 
-          {param_specs, acc} =
-            params
-            |> Enum.map_reduce(MapSet.new(), fn p, acc ->
-              {spec, acc} =  build_param_spec(p.schema, component.expandable_fields, acc)
-              res = if p.required do
-                {quote do
-                   unquote(p.name |> String.to_atom())
-                 end, spec}
+          {param_specs, object_types} = unnest_object_types(params)
+
+          object_types = MapSet.to_list(object_types)
+
+          ast =
+            quote do
+              if unquote(operation_definition.deprecated) do
+                @deprecated "Stripe has deprecated this operation"
+              end
+
+              @operation unquote(Macro.escape(operation_definition))
+              @doc unquote(operation_definition.description)
+
+              if unquote(params?) do
+                @spec unquote(function_name)(
+                        client :: term(),
+                        unquote_splicing(argument_specs),
+                        params :: unquote(to_inline_spec(param_specs))
+                      ) ::
+                        {:ok, unquote(success_response_spec)}
+                        | {:error, Stripe.ApiErrors.t()}
+                        | {:error, term()}
+
+                def unquote(function_name)(
+                      client,
+                      unquote_splicing(argument_names),
+                      params \\ %{}
+                    ) do
+                  path =
+                    Stripe.OpenApi.Path.replace_path_params(
+                      @operation.path,
+                      @operation.path_parameters,
+                      unquote(argument_values)
+                    )
+
+                  Stripe.request(@operation.method, path, client, params || %{})
+                end
               else
-                {quote do
-                   optional(unquote(p.name |> String.to_atom()))
-                 end, spec}
-              end
-              {res, acc}
-            end)
+                @spec unquote(function_name)(client :: term(), unquote_splicing(argument_specs)) ::
+                        {:ok, unquote(success_response_spec)}
+                        | {:error, Stripe.ApiErrors.t()}
+                        | {:error, term()}
+                def unquote(function_name)(
+                      client,
+                      unquote_splicing(argument_names)
+                    ) do
+                  path =
+                    Stripe.OpenApi.Path.replace_path_params(
+                      @operation.path,
+                      @operation.path_parameters,
+                      unquote(argument_values)
+                    )
 
-            IO.inspect(acc)
-          quote do
-            if unquote(operation_definition.deprecated) do
-              @deprecated "Stripe has deprecated this operation"
-            end
-
-            @operation unquote(Macro.escape(operation_definition))
-            @doc unquote(operation_definition.description)
-
-            if unquote(params?) do
-              @spec unquote(function_name)(
-                      client :: term(),
-                      unquote_splicing(argument_specs),
-                      params
-                    ) ::
-                      {:ok, unquote(success_response_spec)}
-                      | {:error, Stripe.ApiErrors.t()}
-                      | {:error, term()}
-                    when params: %{unquote_splicing(param_specs)}
-              def unquote(function_name)(
-                    client,
-                    unquote_splicing(argument_names),
-                    params \\ %{}
-                  ) do
-                path =
-                  Stripe.OpenApi.Path.replace_path_params(
-                    @operation.path,
-                    @operation.path_parameters,
-                    unquote(argument_values)
-                  )
-
-                Stripe.request(@operation.method, path, client, params || %{})
-              end
-            else
-              @spec unquote(function_name)(client :: term(), unquote_splicing(argument_specs)) ::
-                      {:ok, unquote(success_response_spec)}
-                      | {:error, Stripe.ApiErrors.t()}
-                      | {:error, term()}
-              def unquote(function_name)(
-                    client,
-                    unquote_splicing(argument_names)
-                  ) do
-                path =
-                  Stripe.OpenApi.Path.replace_path_params(
-                    @operation.path,
-                    @operation.path_parameters,
-                    unquote(argument_values)
-                  )
-
-                Stripe.request(@operation.method, path, client, %{})
+                  Stripe.request(@operation.method, path, client, %{})
+                end
               end
             end
-          end
+
+          {ast, object_types}
         end
 
+      {funcs, types} = Enum.unzip(funcs_types)
       fields = component.properties |> Map.keys() |> Enum.map(&String.to_atom/1)
+
+      # TODO fix  uniq
+      types =
+        List.flatten(types)
+        |> Enum.uniq_by(fn {_, meta, _} -> meta[:name] end)
+        |> Enum.map(&to_type_spec/1)
 
       specs =
         Enum.map(component.properties, fn {key, value} ->
@@ -146,16 +148,9 @@ defmodule Stripe.OpenApi.Phases.Compile do
       #{typedoc_fields}
       """
 
-      description =
-        if funcs == [] do
-          component.description
-        else
-          component.description
-        end
-
       body =
         quote do
-          @moduledoc unquote(description)
+          @moduledoc unquote(component.description)
           if unquote(fields) != nil do
             @derive {Inspect, optional: unquote(fields)}
             defstruct unquote(fields)
@@ -166,6 +161,8 @@ defmodule Stripe.OpenApi.Phases.Compile do
                   }
           end
 
+          unquote_splicing(types)
+
           (unquote_splicing(funcs))
         end
 
@@ -173,6 +170,149 @@ defmodule Stripe.OpenApi.Phases.Compile do
     end
 
     {:ok, blueprint}
+  end
+
+  defp unnest_object_types(params) do
+    Macro.postwalk(params, MapSet.new(), fn
+      {:nullable, meta, [{:object, _meta, children}]}, acc ->
+        if meta[:name] == nil || children == [] do
+          {{:object, meta, children}, acc}
+        else
+          {{:ref, [name: meta[:name]], []}, MapSet.put(acc, {:object, meta, children})}
+        end
+
+      {:object, meta, children}, acc ->
+        if meta[:name] == nil || children == [] do
+          {{:object, meta, children}, acc}
+        else
+          {{:ref, [name: meta[:name]], []}, MapSet.put(acc, {:object, meta, children})}
+        end
+
+      other, acc ->
+        {other, acc}
+    end)
+  end
+
+  defp to_type_spec({:object, meta, children}) do
+    specs = Enum.map(children, &to_spec_map/1)
+
+    name = type_spec_name(meta[:name])
+
+    quote do
+      @typedoc unquote(meta[:description])
+      @type unquote(Macro.var(name, __MODULE__)) :: %{
+              unquote_splicing(specs)
+            }
+    end
+  end
+
+  defp to_type_spec({:array, meta, [child]} = _ast) do
+    name = type_spec_name(meta[:name])
+
+    quote do
+      @typedoc unquote(meta[:description])
+      @type unquote(Macro.var(name, __MODULE__)) :: unquote(to_type(child))
+    end
+  end
+
+  defp to_type_spec({_, meta, children}) do
+    specs = Enum.map(children, &to_spec_map/1)
+
+    name = type_spec_name(meta[:name])
+
+    quote do
+      @typedoc unquote(meta[:description])
+      @type unquote(Macro.var(name, __MODULE__)) :: %{
+              unquote_splicing(specs)
+            }
+    end
+  end
+
+  defp type_spec_name(name) do
+    if name in [:reference] do
+      :reference_0
+    else
+      name
+    end
+  end
+
+  defp to_inline_spec({_, _meta, children}) do
+    specs = Enum.map(children, &to_spec_map/1)
+
+    quote do
+      %{
+        unquote_splicing(specs)
+      }
+    end
+  end
+
+  defp to_spec_map({:array, meta, [_type]} = ast) do
+    {meta[:name], to_type(ast)}
+  end
+
+  defp to_spec_map({:any_of, meta, [type | tail]}) do
+    {meta[:name],
+     quote do
+       unquote(to_type(type)) | unquote(to_type(tail))
+     end}
+  end
+
+  defp to_spec_map({:ref, meta, _} = ast) do
+    {meta[:name], to_type(ast)}
+  end
+
+  defp to_spec_map({:nullable, meta, [type]}) do
+    {quote do
+       optional(unquote(meta[:name]))
+     end, to_type(type)}
+  end
+
+  defp to_spec_map({_type, meta, _children} = ast) do
+    {meta[:name], to_type(ast)}
+  end
+
+  def to_type([type]) do
+    quote do
+      unquote(to_type(type))
+    end
+  end
+
+  def to_type([type | tail]) do
+    quote do
+      unquote(to_type(type)) | unquote(to_type(tail))
+    end
+  end
+
+  def to_type({:ref, meta, _}) do
+    Macro.var(meta[:name], __MODULE__)
+  end
+
+  def to_type({:array, _meta, [type]}) do
+    quote do
+      list(unquote(to_type(type)))
+    end
+  end
+
+  def to_type({:any_of, _, [type | tail]}) do
+    quote do
+      unquote(to_type(type)) | unquote(to_type(tail))
+    end
+  end
+
+  def to_type({:string, metadata, _}) do
+    if metadata[:enum] do
+      to_type(metadata[:enum])
+    else
+      :string
+    end
+  end
+
+  def to_type({type, _, _}) do
+    type
+  end
+
+  def to_type(type) do
+    type
   end
 
   defp return_spec(%OpenApiGen.Blueprint.Reference{name: name}) do
@@ -257,99 +397,6 @@ defmodule Stripe.OpenApi.Phases.Compile do
   defp build_spec(_, _) do
     quote do
       term
-    end
-  end
-
-  defp build_param_spec(%{type: "string"}, _, acc) do
-    {quote do
-      binary
-    end, acc}
-  end
-
-  defp build_param_spec(%{type: "integer"}, _, acc) do
-    {quote do
-      integer
-    end, acc}
-  end
-
-  defp build_param_spec(%{type: "number"}, _, acc) do
-    {quote do
-      integer | float
-    end, acc}
-  end
-
-  defp build_param_spec(%{type: "boolean"}, _, acc) do
-    {quote do
-      boolean
-    end, acc}
-  end
-
-  defp build_param_spec(%{type: "object", properties: properties, name: name}, expandable_fields, acc) do
-    specs =
-      properties
-      |> Enum.map(fn s ->
-        {quote do
-           optional(unquote(s.name |> String.to_atom()))
-         end, build_param_spec(s, expandable_fields, acc)}
-      end)
-
-    quote do
-      %{
-        unquote_splicing(specs)
-      }
-    end
-
-    if name do
-      {quote do
-        unquote(String.to_atom(name)).t
-      end, MapSet.put(acc, {String.to_atom(name), specs})}
-    else
-      {quote do
-        %{
-          unquote_splicing(specs)
-        }
-      end, acc}
-    end
-
-  end
-
-  defp build_param_spec(%{type: :any_of, any_of: [type]}, expandable_fields, acc) do
-    build_param_spec(type, expandable_fields, acc)
-  end
-
-  defp build_param_spec(%{type: :any_of, any_of: [any_of | tail]} = type, expandable_fields, acc) do
-    type = Map.put(type, :any_of, tail)
-
-    {any_of, acc1} = build_param_spec(any_of, expandable_fields, acc)
-    {tail, acc} = build_param_spec(type, expandable_fields, acc)
-    {{:|, [],
-     [any_of, tail]}, MapSet.union(acc1, acc)}
-  end
-
-  defp build_param_spec(%{type: "array", items: schema, name: "expand"}, expandable_fields, acc) do
-    {quote do
-      [binary | unquote(expandable_fields(expandable_fields))]
-    end, acc}
-  end
-
-  defp build_param_spec(%{type: "array", items: schema}, expandable_fields, acc) do
-    {spec, acc} = build_param_spec(schema, expandable_fields, acc)
-    {quote do
-      [unquote(spec)]
-    end, acc}
-  end
-
-  defp expandable_fields([]) do
-    []
-  end
-
-  defp expandable_fields([field]) do
-    field
-  end
-
-  defp expandable_fields([field | tail]) do
-    quote do
-      unquote(field) | unquote(expandable_fields(tail))
     end
   end
 
